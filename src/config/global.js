4d3c4bf094db994853272ae96cafad1b0f96b19a
Francisco Lizcano <flizcanor@icloud.com>
2025-09-21 21:51:19 -0500

    Home

--- src/config/global.js
+++ src/config/global.js
@@ -1,7 +1,7 @@
 export default {
   global: {
     numeroUnidad: '3',
-    tituloUnidad: 'Bioética y sostenibilidad en el desarrollo económico global',
+    tituloUnidad: 'Educación del siglo XXI',
   },
   menuPrincipal: {
     menu: [
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -22,11 +22,9 @@
             .content-wrapper.px-lg-4.pt-lg-4.px-xxl-5.py-xxl-5.text-start
               p.home-main-title UNIDAD {{ `${globalData.numeroUnidad}` }}
               h1.home-title.mb-4 {{ globalData.tituloUnidad }}
-              p.descripcion.mb-4 Desde los comienzos de la humanidad, la relación entre los principios éticos, el desarrollo económico y la sostenibilidad ha sido fundamental para garantizar un progreso equilibrado y equitativo. En un contexto globalizado, donde los desafíos éticos y ambientales son cada vez más evidentes, la bioética emerge como una herramienta clave para orientar decisiones económicas responsables que respeten tanto el medio ambiente como los derechos humanos.
+              p.descripcion.mb-4 Desde sus raíces más tradicionales hasta sus expresiones más actuales, la pedagogía ha evolucionado en respuesta a los cambios sociales, culturales y tecnológicos de cada época. En esta tercera unidad, se explorarán los fundamentos de la pedagogía contemporánea y su impacto en los procesos de enseñanza y aprendizaje. A través del análisis de enfoques como el pensamiento crítico, la educación popular, el aprendizaje situado, la pedagogía para la paz y las prácticas emergentes en el aula, se busca ampliar la comprensión del rol del educador en contextos diversos y dinámicos.
 
-              p.descripcion.mb-4 En esta unidad, profundizaremos en el rol de la bioética como pilar de la sostenibilidad en el desarrollo económico, explorando temas esenciales como la aplicación de principios éticos al uso de recursos naturales, la influencia de las políticas globales en la protección de los derechos humanos y la ética en la innovación tecnológica. 
-              
-              p #[b Prepárate para explorar los retos y oportunidades que la sostenibilidad y la bioética presentan en un mundo en constante evolución.]
+              p #[b ¡Prepárese para transformar su práctica educativa con herramientas pedagógicas actuales y pertinentes para el siglo XXI!]
 
               .button-container.mt-auto
                 router-link.btn-iniciar.w-100.w-md-auto(:to="{ name: 'introduccion' }")
